0fbd4b60ea5f75f42198d801a191030c34635242
Emil Dimanchev <emild@mit.edu>
2025-11-18 11:10:52 -0500

    removing old  case:

--- ExampleSystems/wecc_11z_30p_1cra/Run_oncluster.jl
+++
@@ -1,9 +0,0 @@
-case = dirname(@__FILE__);
-
-import Pkg
-# Pkg.instantiate()
-# println("Activating Package")
-
-Pkg.activate("/home/ed0400/1_Macro")
-
-include("/home/ed0400/1_Macro/ExampleSystems/wecc_11z_30p/run.jl");
