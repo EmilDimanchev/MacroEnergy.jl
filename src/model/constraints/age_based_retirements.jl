c10a0426cf9583b366b5c0210a1a6c20808dd88e
Emil Dimanchev <emild@mit.edu>
2025-12-02 17:05:02 -0500

    fixing retirement problem with can retire false

--- src/model/constraints/age_based_retirements.jl
+++ src/model/constraints/age_based_retirements.jl
@@ -12,10 +12,12 @@ function add_model_constraint!(ct::AgeBasedRetirementConstraint, y::Union{Abstra
 
 
         #### All new capacity built up to the retirement period must either retire or be retrofitted in the current period
-        ct.constraint_ref = @constraint(
-            model, 
-            sum(new_capacity_track(y,k) for k=1:ret_period;init=0) + min_retired_capacity(y) <= sum(retired_capacity_track(y,k) for k=1:curr_period) + sum(retrofitted_capacity_track(y,k) for k=1:curr_period)
-        )
+        if can_retire(y)
+            ct.constraint_ref = @constraint(
+                model, 
+                sum(new_capacity_track(y,k) for k=1:ret_period;init=0) + min_retired_capacity(y) <= sum(retired_capacity_track(y,k) for k=1:curr_period) + sum(retrofitted_capacity_track(y,k) for k=1:curr_period)
+            )
+        end
     else
     end    
 
