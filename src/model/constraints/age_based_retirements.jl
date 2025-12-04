0b6336f29cf3722ca145a90ed9f9cc21efee01dd
Emil Dimanchev <emild@mit.edu>
2025-12-04 10:01:06 -0500

    removing retirement flag

--- src/model/constraints/age_based_retirements.jl
+++ src/model/constraints/age_based_retirements.jl
@@ -6,20 +6,20 @@ end
 
 function add_model_constraint!(ct::AgeBasedRetirementConstraint, y::Union{AbstractEdge,AbstractStorage}, model::Model, settings::NamedTuple)
     
-    if settings[:retirement]
-        curr_period = period_index(y);
-        ret_period = retirement_period(y);
+
+    curr_period = period_index(y);
+    ret_period = retirement_period(y);
 
 
-        #### All new capacity built up to the retirement period must either retire or be retrofitted in the current period
-        if can_retire(y)
-            ct.constraint_ref = @constraint(
-                model, 
-                sum(new_capacity_track(y,k) for k=1:ret_period;init=0) + min_retired_capacity(y) <= sum(retired_capacity_track(y,k) for k=1:curr_period) + sum(retrofitted_capacity_track(y,k) for k=1:curr_period)
-            )
-        end
-    else
-    end    
+    #### All new capacity built up to the retirement period must either retire or be retrofitted in the current period
+    if can_retire(y)
+        ct.constraint_ref = @constraint(
+            model, 
+            sum(new_capacity_track(y,k) for k=1:ret_period;init=0) + min_retired_capacity(y) <= sum(retired_capacity_track(y,k) for k=1:curr_period) + sum(retrofitted_capacity_track(y,k) for k=1:curr_period)
+        )
+    end
+
+
 
     return nothing
 end
