4ebaeb4d8f54d3c190216d483006a2ea9152b107
Emil Dimanchev <emild@mit.edu>
2026-01-22 19:39:07 -0500

    tests

--- ExampleSystems/thirtyperiods_electricity_only_new_england_multistage/run_benders.jl
+++ ExampleSystems/thirtyperiods_electricity_only_new_england_multistage/run_benders.jl
@@ -1,11 +1,3 @@
-# using MacroEnergy
-# using Gurobi
-
-# (system, results) = run_case(@__DIR__;
-#     planning_optimizer=Gurobi.Optimizer,
-#     subproblem_optimizer=Gurobi.Optimizer,
-#     planning_optimizer_attributes=("Method" => 2, "Crossover" => 0, "BarConvTol" => 1e-3, "BarHomogeneous" => 1, "MIPFocus" => 3),
-#     subproblem_optimizer_attributes=("Method" => 2, "Crossover" => 1, "BarConvTol" => 1e-3));
 using MacroEnergy
 using Gurobi
 
